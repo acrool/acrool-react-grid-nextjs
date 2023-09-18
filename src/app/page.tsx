ac8aca094027088e24f27d52aa1b41ed84edb2aa
imagine10255 <imagine10255@gmail.com>
2023-09-18 23:43:17 +0800

    add bear-react-grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,108 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import Image from 'next/image';
+import styles from './page.module.css';
+import styled from 'styled-components';
+import {Row, Grid, Col, Container} from 'bear-react-grid';
+
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+
+    const renderMain = () => {
+        return <Grid columns="1fr auto 1fr" className="align-items-center mb-5">
+
+            <div className="d-flex justify-content-end">
+                <Image
+                    src="/logo.svg"
+                    alt="BearReactGrid Logo"
+                    width={50}
+                    height={50}
+                    className="mr-3"
+                    priority
+                />
+                <Title>
+                    Bear React Grid
+                </Title>
+            </div>
+
+            <Add/>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+                className={styles.logo}
+                src="/next.svg"
+                alt="Next.js Logo"
+                width={180}
+                height={30}
+                priority
             />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        </Grid>;
+    };
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    const renderLearn = () => {
+        const data = [
+            {
+                text: 'Docs',
+                desc: 'Find in-depth information about Next.js features and API.',
+                url: 'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+            },
+            {
+                text: 'Learn',
+                desc: 'Learn about Next.js in an interactive course with&nbsp;quizzes!',
+                url: 'https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+            },
+            {
+                text: 'Templates',
+                desc: 'Explore the Next.js 13 playground.',
+                url: 'https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+            },
+            {
+                text: 'Deploy',
+                desc: 'Instantly deploy your Next.js site to a shareable URL with Vercel.',
+                url: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+            },
+        ]
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+        return <Grid columns={4}>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            {data.map(row => {
+                return <a key={row.text}
+                    href={row.url}
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        {row.text} <span>-&gt;</span>
+                    </h2>
+                    <p>{row.desc}</p>
+                </a>
+            })}
+
+        </Grid>;
+    }
+
+
+    return (
+        <Container className="d-flex flex-column align-items-center justify-content-center"
+        style={{height: '100vh'}}>
+            {renderMain()}
+            {renderLearn()}
+        </Container>
+    );
 }
+
+
+const Title = styled.div`
+  font-size: 40px;
+  color: #fff;
+`;
+
+const Add = styled.div`
+  font-size: 40px;
+  color: #fff;
+
+  :after {
+    content: '+';
+  }
+`;
---
+++ src/lib/registry.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import React, { useState } from 'react'
+import { useServerInsertedHTML } from 'next/navigation'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+import {GridThemeProvider} from 'bear-react-grid';
+
+const gridTheme = {
+    spacer: '1rem',
+    gridGutterWidth: '.625rem',
+    gridColumns: 24,
+    gridBreakpoints: {
+        xs: 0,
+        sm: 576,
+        md: 768,
+        lg: 992,
+        xl: 1200,
+        xxl: 1540,
+    },
+    containerMaxWidths: {
+        xs: 540,
+        sm: 540,
+        md: 720,
+        lg: 960,
+        xl: 1140,
+        xxl: 1141,
+    },
+}
+
+interface IBearReactGridRegistryProps {
+    children: React.ReactNode,
+}
+
+export function BearReactGridRegistry({
+    children,
+}: IBearReactGridRegistryProps) {
+    return <GridThemeProvider gridTheme={gridTheme}>
+        {children}
+    </GridThemeProvider>;
+}
+
+export default function StyledComponentsRegistry({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    // Only create stylesheet once with lazy initial state
+    // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+    const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
+
+    useServerInsertedHTML(() => {
+        const styles = styledComponentsStyleSheet.getStyleElement()
+        styledComponentsStyleSheet.instance.clearTag()
+        return <>{styles}</>
+    })
+
+    if (typeof window !== 'undefined') return <>{children}</>
+    return (
+        <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+            {children}
+        </StyleSheetManager>
+    )
+}
